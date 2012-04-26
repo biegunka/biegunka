3cc3c93c835a0d7534d032d7edda498bb612a6e2
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-04-27 02:30:50 +0400

    Add DryRun script functions.

--- Biegunka/Script/DryRun.hs
+++ Biegunka/Script/DryRun.hs
@@ -1,11 +1,24 @@
 module Biegunka.Script.DryRun where
 
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Writer (tell)
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
+
 import Biegunka
 
 data Host = Root | Home
 
+dir ∷ Host → IO FilePath
+dir Root = return ""
+dir Home = getHomeDirectory
+
 repoTo ∷ Host → FilePath → Script ()
-repoTo = undefined
+repoTo h fp = Script $ do
+  d <- liftIO (dir h)
+  tell ([d </> fp])
 
 fromRepoTo ∷ Host → (FilePath, FilePath) → Script ()
-fromRepoTo = undefined
+fromRepoTo h (_, fp) = Script $ do
+  d <- liftIO (dir h)
+  tell ([d </> fp])
