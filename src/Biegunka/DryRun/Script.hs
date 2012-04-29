7fefaeb75e8d6ccb196c320ab0c1ec927a314f70
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-04-29 20:21:42 +0400

    Complete first stage of Script module.

--- src/Biegunka/DryRun/Script.hs
+++ src/Biegunka/DryRun/Script.hs
@@ -1,7 +1,9 @@
 module Biegunka.DryRun.Script where
 
 import Control.Monad.Trans (liftIO)
+import Control.Monad.Reader (ask)
 import Control.Monad.Writer (tell)
+import Data.Monoid ((<>))
 import System.Directory (getHomeDirectory)
 import System.FilePath ((</>))
 
@@ -13,10 +15,21 @@ instance ScriptI Script where
   link_repo_file = link_repo_file_
 
 link_repo_itself_ ∷ FilePath → Script ()
-link_repo_itself_ fp = Script $ liftIO getHomeDirectory >>= \d → tell [d </> fp]
+link_repo_itself_ fp = Script $ do
+  s ← ask
+  hd ← liftIO getHomeDirectory
+  let d = hd </> fp
+  liftIO . putStrLn $ "Link " <> s <> " to " <> d
+  tell [d]
 
 link_repo_file_ ∷ FilePath → FilePath → Script ()
-link_repo_file_ _ dst = Script $ liftIO getHomeDirectory >>= \d → tell [d </> dst]
+link_repo_file_ sfp dfp = Script $ do
+  rd ← ask
+  hd ← liftIO getHomeDirectory
+  let s = rd </> sfp
+  let d = hd </> dfp
+  liftIO . putStrLn $ "Link " <> s <> " to " <> d
+  tell [d]
 
 message_ ∷ String → Script ()
-message_ = const (return ())
+message_ _ = return ()
--- src/Biegunka/Script.hs
+++ src/Biegunka/Script.hs
@@ -1,5 +1,15 @@
 module Biegunka.Script where
 
+import Control.Applicative ((<*>), (<$>))
+import Control.Monad (when)
+import Control.Monad.Trans (liftIO)
+import Control.Monad.Reader (ask)
+import Control.Monad.Writer (tell)
+import Data.Monoid ((<>))
+import System.Directory (getHomeDirectory, removeFile)
+import System.Posix.Files (createSymbolicLink, fileExist, removeLink)
+import System.FilePath ((</>))
+
 import Biegunka.Core
 
 instance ScriptI Script where
@@ -8,10 +18,31 @@ instance ScriptI Script where
   link_repo_file = link_repo_file_
 
 link_repo_itself_ ∷ FilePath → Script ()
-link_repo_itself_ = undefined
+link_repo_itself_ fp = Script $ do
+  s ← ask
+  hd ← liftIO getHomeDirectory
+  let d = hd </> fp
+  liftIO $ do
+    exists ← fileExist d
+    when (exists) $ do
+      putStrLn $ "Warning: file " <> d <> " does exist!"
+      removeLink d
+    createSymbolicLink s d
+  tell [d]
 
 link_repo_file_ ∷ FilePath → FilePath → Script ()
-link_repo_file_ = undefined
+link_repo_file_ sfp dfp = Script $ do
+  rd ← ask
+  hd ← liftIO getHomeDirectory
+  let s = rd </> sfp
+  let d = hd </> dfp
+  liftIO $ do
+    exists ← fileExist d
+    when (exists) $ do
+      putStrLn $ "Warning: file " <> d <> " does exist!"
+      removeLink d
+    createSymbolicLink s d
+  tell [d]
 
 message_ ∷ String → Script ()
-message_ = undefined
+message_ = Script . liftIO . putStrLn
