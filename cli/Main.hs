b62f30e0af40be141e35d8f0a0b25153a20dc602
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-08-08 23:57:03 +0400

    Support profiles in Biegunka DB.

--- cli/Main.hs
+++ cli/Main.hs
@@ -16,20 +16,39 @@ main = execParser parserInfo >>= doBiegunka
 
 data Options = Options
   { wipe ∷ Bool
-  , remove ∷ FilePath
+  , profile :: FilePath
+  , repository ∷ FilePath
   , file ∷ FilePath
   } deriving Show
 
 
 parser ∷ Parser Options
 parser = Options
-  <$> switch (long "wipe" . help "Remove everything Biegunka has installed." . value False)
-  <*> strOption (long "remove" . metavar "REPO" . help "Remove from repository REPO" . value "")
-  <*> strOption (long "file" . metavar "FILE" . help "Remove only this FILE from specified REPO" . value "")
+  <$> switch (long "wipe"
+        . help "Remove everything Biegunka has installed."
+        . value False)
+  <*> strOption (long "profile"
+        . metavar "PROFILE"
+        . help "Remove from profile PROFILE"
+        . value "")
+  <*> strOption (long "repository"
+        . metavar "REPO"
+        . help "Remove from repository REPO"
+        . value "")
+  <*> strOption (long "file"
+        . metavar "FILE"
+        . help "Remove only this FILE from specified REPO"
+        . value "")
 
 
 doBiegunka ∷ Options → IO ()
-doBiegunka Options { wipe = True } = B.withBiegunka B.wipe
-doBiegunka Options { remove = r@(_:_), file = [] } = B.withBiegunka (B.purge r)
-doBiegunka Options { remove = r@(_:_), file = f } = B.withBiegunka (B.delete r f)
-doBiegunka _ = putStrLn "You should specify either a REPO or REPO+FILE or --wipe alltogether."
+doBiegunka Options { wipe = True } =
+  B.withBiegunka B.wipe
+doBiegunka Options { profile = p@(_:_), repository = "", file = "" } =
+  B.withBiegunka $ B.removeProfile p
+doBiegunka Options { profile = p@(_:_), repository = r@(_:_), file = "" } =
+  B.withBiegunka $ B.removeRepo p r
+doBiegunka Options { profile = p@(_:_), repository = r@(_:_), file = f } =
+  B.withBiegunka $ B.removeFile p r f
+doBiegunka _ =
+  putStrLn "You should specify either a PROFILE or PROFILE+REPO or PROFILE+REPO+FILE or --wipe alltogether."
--- src/Biegunka/DB.hs
+++ src/Biegunka/DB.hs
@@ -6,19 +6,23 @@
 module Biegunka.DB
   ( Biegunka
   , withBiegunka
-  , create, merge, delete, purge, wipe
+  , create, merge
+  , removeFile, removeRepo, removeProfile, wipe
   ) where
 
 import Control.Applicative ((<$>), empty, liftA2)
 import Control.Monad (when)
+import Data.Foldable (fold, foldMap, mapM_)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Monoid(..))
+import Prelude hiding (mapM_)
 import System.IO (IOMode(ReadMode), withFile)
 
 import Data.Aeson hiding (encode)
 import Data.Aeson.Encode.Pretty
 import qualified Data.ByteString.Lazy as B
-import System.Directory (getHomeDirectory, doesFileExist, removeDirectoryRecursive, removeFile)
+import System.Directory (getHomeDirectory, doesFileExist, removeDirectoryRecursive)
+import qualified System.Directory as D
 import System.FilePath ((</>))
 import Data.Map (Map, (!))
 import qualified Data.Map as M
@@ -29,20 +33,20 @@ import qualified Data.Set as S
 -- | The result of the installation script.
 -- Contains a map of repository paths (keys) to installed files (values)
 newtype Biegunka =
-  Biegunka { φ ∷ Map FilePath (Set FilePath) } deriving (Monoid, Show)
+  Biegunka (Map String (Map FilePath (Set FilePath))) deriving (Show, Monoid)
 
 
 instance FromJSON Biegunka where
-  parseJSON (Object o) = Biegunka . M.fromList <$>
+  parseJSON (Object o) = Biegunka . M.singleton "default" . M.fromList <$>
     (o .: "repo" >>= mapM (\r → liftA2 (,) (r .: "path") (S.fromList <$> r .: "files")))
   parseJSON _ = empty
 instance ToJSON Biegunka where
-  toJSON (Biegunka α) = object [ "repo" .= map (\(k,v) → object ["path" .= k, "files" .= S.toList v]) (M.toList α) ]
+  toJSON (Biegunka α) = object [ "repo" .= map (\(k,v) → object ["path" .= k, "files" .= S.toList v]) (M.toList $ α ! "default") ]
 
 
 -- | Create an Biegunka for a repostory.
 create ∷ FilePath → Set FilePath → Biegunka
-create fp = Biegunka . M.singleton fp
+create fp = Biegunka . M.singleton "default" . M.singleton fp
 
 
 -- | Load a Biegunka from ~/.biegunka.db, do any actions with it, save it back.
@@ -64,52 +68,76 @@ withBiegunka action = load >>= action >>= save
 
 -- | Merge (sum) two Biegunkas.
 merge ∷ Biegunka → Biegunka → Biegunka
-merge (φ → α) (φ → β) = Biegunka $ M.unionWith mappend α β
+merge (Biegunka α) (Biegunka β) = Biegunka $ M.unionWith mappend α β
 
 
--- | Delete an installed file related to specified repository.
+-- | Delete an installed file related to specified repository in given profile.
 --
 -- Algorithm is as follows:
--- 1. adjust current map by key `rp' with set with deleted element `fp'
---    note: both Data.Map.adjust and Data.Set.delete return the same data struct on fail
--- 2. if adjusted map is not the same with initial (which means Data.Map.adjust and
---    Data.Set.delete both have succeed) remove file `fp`
--- 3. return new map (either it's old one or really adjusted)
-delete ∷ FilePath → FilePath → Biegunka → IO Biegunka
-delete rp fp (φ → o) =
-  do let n = M.adjust (S.delete fp) rp o
-     when (n /= o) $ removeFile fp
-     return $ Biegunka n
+-- 1. Check if given profile is known
+-- 2. If it is then adjust current map by key `rp' with set with deleted element `fp'
+--    note: both Data.Map.adjust and Data.Set.delete return the unchanged data struct on fail
+-- 3. If adjusted map is changed (which means Data.Map.adjust and
+--    Data.Set.delete both have succeed) remove file `fp'
+-- 4. Update map in given profile
+removeFile ∷ String → FilePath → FilePath → Biegunka → IO Biegunka
+removeFile profile repo filepath b@(Biegunka current) =
+  case M.lookup profile current of
+    Just profileFiles →
+      do let adjustedProfileFiles = M.adjust (S.delete filepath) repo profileFiles
+         when (adjustedProfileFiles /= profileFiles) $ D.removeFile filepath
+         return $ Biegunka $ M.insert profile adjustedProfileFiles current
+    Nothing → return b
 
 
--- | Purge all installed files related to specified repository.
+-- | Remove all installed files related to specified repository.
 --
 -- Algorithm is as follows:
--- 1. delete from current map by key `rp'
---    note: Data.Map.delete returns the same data struct on fail
--- 2. if adjusted map is not the same with initial (which means Data.Map.delete
---    has succeed) remove all files by key `rp` in the initial map
--- 3. return new map (either it's old one or really adjusted)
-purge ∷ FilePath → Biegunka → IO Biegunka
-purge rp (φ → o) =
-  do let n = M.delete rp o
-         xs = S.toList $ o ! rp
-     when (n /= o) $
-       do mapM_ removeFile xs
-          removeDirectoryRecursive rp
-     return $ Biegunka n
+-- 1. Check if given profile is known
+-- 2. If it is then delete from current map by key `rp'
+--    note: Data.Map.delete returns unchanged data struct on fail
+-- 2. If adjusted map is changed (which means Data.Map.delete
+--    has succeed) remove all files by key `rp' in the initial map
+-- 4. Update map in given profile
+removeRepo ∷ String → FilePath → Biegunka → IO Biegunka
+removeRepo profile repo b@(Biegunka current) =
+  case M.lookup profile current of
+    Just profileFiles →
+      do let adjustedProfileFiles = M.delete repo profileFiles
+         when (adjustedProfileFiles /= profileFiles) $
+           do mapM_ D.removeFile $ profileFiles ! repo
+              removeDirectoryRecursive repo
+         return $ Biegunka $ M.insert profile adjustedProfileFiles current
+    Nothing → return b
+
+
+-- | Remove all installed repositories related to specified profile.
+--
+-- Algorithm is as follows:
+-- 1. Check if given profile is known
+-- 2. If it is then delete from current map by key `rp'
+--    note: Data.Map.delete returns unchanged data struct on fail
+-- 2. If adjusted map is changed (which means Data.Map.delete
+--    has succeed) remove all files by key `rp' in the initial map
+-- 4. Update map in given profile
+removeProfile ∷ String → Biegunka → IO Biegunka
+removeProfile profile b@(Biegunka current) =
+  case M.lookup profile current of
+    Just profileFiles →
+      do mapM_ D.removeFile $ fold profileFiles
+         mapM_ removeDirectoryRecursive $ M.keysSet profileFiles
+         return $ Biegunka $ M.delete profile current
+    Nothing → return b
 
 
 -- | Wipe all repositories.
 --
 -- Algorithm is as follows:
--- 1. fold current map to list of files
--- 2. remove every file in the list
--- 3. no point to return something: resulting map is merely the Data.Map.empty one
+-- 1. Fold current map to set of files
+-- 2. Remove every file in the set
+-- 3. No point to return something: resulting map is merely the Data.Map.empty one
 wipe ∷ Biegunka → IO Biegunka
-wipe (φ → db) =
-  do let xs = S.toList $ M.fold mappend S.empty db
-         ys = M.keys db
-     mapM_ removeFile xs
-     mapM_ removeDirectoryRecursive ys
+wipe (Biegunka current) =
+  do mapM_ D.removeFile $ foldMap fold current
+     mapM_ removeDirectoryRecursive $ foldMap M.keysSet current
      return $ Biegunka mempty
