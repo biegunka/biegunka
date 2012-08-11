5d2772dec49b1bcd1712c5cfdae7c066667059ab
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-08-11 20:20:11 +0400

    Support multiple definitions of the same profile in the single script. #1.

--- src/Biegunka/Interpreter/Execute/Profile.hs
+++ src/Biegunka/Interpreter/Execute/Profile.hs
@@ -1,7 +1,6 @@
 {-# OPTIONS_HADDOCK hide #-}
 module Biegunka.Interpreter.Execute.Profile (execute) where
 
-import Control.Applicative ((<$>))
 import qualified Data.Map as M
 import Data.Monoid (Monoid(..))
 
@@ -15,6 +14,7 @@ import qualified Biegunka.Interpreter.Execute.Repository as Repository
 
 execute ∷ Free (Profile a) b → IO (Map String (Map FilePath (Set FilePath)))
 execute (Free (Profile name repo next)) =
-  do biegunka ← Repository.execute repo
-     mappend (M.singleton name biegunka) <$> execute next
+  do α ← Repository.execute repo
+     β ← execute next
+     return $ M.insertWith' mappend name α β
 execute (Pure _) = return mempty
