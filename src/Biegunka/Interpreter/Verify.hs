b8528507e5fc1adc57f231690c4fb5e490c66e41
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-08-24 00:36:38 +0400

    Move verification failures log to WriterT. #7

--- src/Biegunka/Interpreter/Verify.hs
+++ src/Biegunka/Interpreter/Verify.hs
@@ -1,11 +1,13 @@
 {-# OPTIONS_HADDOCK hide #-}
 module Biegunka.Interpreter.Verify (verify) where
 
-import Control.Applicative (liftA2)
+import Control.Applicative (Applicative, liftA2)
 import Control.Monad (unless)
 
 import           Control.Monad.Free (Free(..))
 import           Control.Monad.State (StateT, evalStateT)
+import           Control.Monad.Writer (WriterT, runWriterT, tell)
+import           Control.Monad.Trans (liftIO)
 import qualified Data.ByteString.Lazy as B
 import           System.Directory (doesDirectoryExist, doesFileExist, getHomeDirectory)
 import           System.Posix.Files (readSymbolicLink)
@@ -20,56 +22,69 @@ verify ∷ StateT BiegunkaState (Free Profile) () → IO ()
 verify script = do
   home ← getHomeDirectory
   let state = BiegunkaState { _root = home, _repositoryRoot = ""}
-  verified ← profile state (evalStateT script state)
+  (verified, failures) ← runWriterT (profile state (evalStateT script state))
   putStr "Verified... "
   if verified
-    then putStrLn "OK"
-    else putStrLn "Fail!"
+    then do
+      putStrLn "OK"
+      putStrLn failures
+    else do
+      putStrLn "Fail!\n"
+      putStrLn failures
 
 
-profile ∷ BiegunkaState → Free Profile () → IO Bool
+profile ∷ BiegunkaState → Free Profile () → WriterT String IO Bool
 profile state (Free (Profile _ script next)) =
   repo state (evalStateT script state) |&&| profile state next
 profile _ (Pure _) = return True
 
 
-repo ∷ BiegunkaState → Free Repository () → IO Bool
+repo ∷ BiegunkaState → Free Repository () → WriterT String IO Bool
 repo state (Free (Git _ path script next)) = do
-  repoExists ← doesDirectoryExist path
+  repoExists ← io $ doesDirectoryExist path
   if repoExists
     then files (evalStateT script state { _repositoryRoot = path }) |&&| repo state next
     else do
-      putStrLn $ "  Repository " ++ path ++ " does not exist"
+      tellLn $ "  Repository " ++ path ++ " does not exist"
       return False |&&| repo state next
 repo _ (Pure _) = return True
 
 
-files ∷ Free Files () → IO Bool
+files ∷ Free Files () → WriterT String IO Bool
 files (Free (Message _ next)) = files next
 files (Free (RegisterAt _ dst next)) = do
-  repoExists ← doesDirectoryExist dst
-  unless repoExists (putStrLn $ "    Repository link at " ++ dst ++ " does not exist")
+  repoExists ← io $ doesDirectoryExist dst
+  unless repoExists (tellLn $ "    Repository link at " ++ dst ++ " does not exist")
   return repoExists |&&| files next
 files (Free (Link src dst next)) = do
-  src' ← readSymbolicLink dst
-  let correctLink = src == src'
-  unless correctLink (putStrLn brokenLink)
+  src' ← io $ readSymbolicLink dst
+  dstExists ← io $ (liftA2 (||) (doesFileExist src') (doesDirectoryExist src'))
+  let correctLink = src == src' && dstExists
+  unless correctLink (tellLn brokenLink)
   return correctLink |&&| files next
  where
-  brokenLink = "    Link at " ++ dst ++ " is broken/does not exist"
+  brokenLink = "    Link at " ++ dst ++ " is broken/destination does not exist"
 files (Free (Copy src dst next)) = do
-  src' ← B.readFile src
-  dst' ← B.readFile dst
+  src' ← io $ B.readFile src
+  dst' ← io $ B.readFile dst
   let same = src' == dst'
-  unless same (putStrLn $ "    Files at " ++ src ++ " and " ++ dst ++ " are not copies")
+  unless same (tellLn $ "    Files at " ++ src ++ " and " ++ dst ++ " are not copies")
   return same |&&| files next
 files (Free (Compile _ _ dst next)) = do
-  binaryExists ← doesFileExist dst
-  unless binaryExists (putStrLn $ "    Compiled binary file at " ++ dst ++ " does not exist")
+  binaryExists ← io $ doesFileExist dst
+  unless binaryExists (tellLn $ "    Compiled binary file at " ++ dst ++ " does not exist")
   return binaryExists |&&| files next
 files (Pure _) = return True
 
 
-(|&&|) ∷ IO Bool → IO Bool → IO Bool
+(|&&|) ∷ Applicative m ⇒ m Bool → m Bool → m Bool
 (|&&|) = liftA2 (&&)
 infixr 3 |&&|
+
+
+io ∷ IO a → WriterT String IO a
+io = liftIO
+
+
+tellLn ∷ String → WriterT String IO ()
+tellLn failure = tell $ failure ++ "\n"
