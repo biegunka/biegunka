f13940d06bfe972e01dfabebf9a6d97098341bd2
Matvey Aksenov <matvey.aksenov@gmail.com>
2012-10-10 00:15:12 +0400

    Add simple type aliases.

--- src/Biegunka.hs
+++ src/Biegunka.hs
@@ -12,6 +12,7 @@ module Biegunka
   , root, sourceRoot, setting, template
     -- * Convenient type aliases
   , ProfileScript, SourceScript, FileScript
+  , SimpleProfileScript, SimpleSourceScript, SimpleFileScript
   ) where
 
 import Biegunka.DSL
@@ -26,3 +27,15 @@ import Biegunka.Settings (root, sourceRoot, setting, template)
 import Biegunka.Interpreter.Pretend (pretend)
 import Biegunka.Interpreter.Execute (execute)
 import Biegunka.Interpreter.Verify (verify)
+
+
+-- | Type alias of ProfileScript for those who doesn't care about customs
+type SimpleProfileScript a = ProfileScript () () a
+
+
+-- | Type alias of SourceScript for those who doesn't care about customs
+type SimpleSourceScript a = SourceScript () () a
+
+
+-- | Type alias of FileScript for those who doesn't care about customs
+type SimpleFileScript a = FileScript () () a
