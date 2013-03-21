ae3c30dd334912cab8778f114028f12b8dfeaebb
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-03-22 01:12:18 +0400

    Update docs.

--- src/Biegunka/Verify.hs
+++ src/Biegunka/Verify.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- | Verification interpreter
 module Biegunka.Verify (verify) where
 
 import Control.Applicative
@@ -19,24 +20,17 @@ import Biegunka.Control (Interpreter(..), logger)
 import Biegunka.Language (IL(..), A(..))
 
 
--- | Verify interpreter
+-- | Verification interpreter
 --
--- Compares current filesystem state and what is written to be executed in script
---
--- May be useful to check if Execute works correctly or too see if you really need to run this script
---
--- @
--- main :: IO ()
--- main = verify $ do
---   profile ...
---   profile ...
--- @
+-- Compares current filesystem layout and what script says it should be line by line.
+-- Outputs errors it find, otherwise prints OK. Is useful to check execution correctness.
 verify :: Interpreter
 verify = I $ \c s -> do
   (verified, failures) <- runWriterT (verification s)
   view logger c $ (if verified then green "OK" else vcat failures) <> line
 
 
+-- | Check layout correctness instruction by instruction creating failures log line by line
 verification :: [IL] -> WriterT [TermDoc] IO Bool
 verification = foldl' (\a -> liftA2 (&&) a . go) (return True)
  where
@@ -44,6 +38,7 @@ verification = foldl' (\a -> liftA2 (&&) a . go) (return True)
     Just l  -> liftIO (correct i `mplus` return False) >>= \c -> unless c (tell [l]) >> return c
     Nothing -> return True
 
+-- | Check single instruction correctness
 correct :: IL -> IO Bool
 correct (IS p _ _ _ _ _) = doesDirectoryExist p
 correct (IA (Link s d) _ _ _) = do
@@ -58,6 +53,7 @@ correct (IA (Copy s d) _ _ _) = do
 correct (IA (Template _ d _) _ _ _) = doesFileExist d
 correct _ = return True
 
+-- | Log message on failure
 log :: IL -> Maybe TermDoc
 log (IS p t _ _ _ u) =
   Just $ text t </> "source" </> parens (cyan (text u)) </> "does not exist at" </> magenta (text p)
