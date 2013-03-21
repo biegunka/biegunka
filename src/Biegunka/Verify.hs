eb68b7e10401f26c247864103fceeb235e245e4f
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-03-22 00:54:58 +0400

    Move Verify interpreter to logger.

--- src/Biegunka/Verify.hs
+++ src/Biegunka/Verify.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Biegunka.Verify (verify) where
 
 import Control.Applicative
-import Control.Monad (unless)
+import Control.Monad (mplus, unless)
 import Data.List (foldl')
-import Data.Monoid (mconcat)
+import Prelude hiding (log)
 
+import           Control.Lens
 import           Control.Monad.Writer (WriterT, runWriterT, tell)
 import           Control.Monad.Trans (liftIO)
 import qualified Data.ByteString.Lazy as B
 import           System.Directory (doesDirectoryExist, doesFileExist)
 import           System.Posix.Files (readSymbolicLink)
+import           Text.PrettyPrint.Free
+import           System.Console.Terminfo.PrettyPrint
 
-import Biegunka.Control (Interpreter(..))
+import Biegunka.Control (Interpreter(..), logger)
 import Biegunka.Language (IL(..), A(..))
 
 
@@ -28,41 +32,39 @@ import Biegunka.Language (IL(..), A(..))
 --   profile ...
 -- @
 verify :: Interpreter
-verify = I $ \_ s -> do
-  (verified, failures) <- runWriterT (f s)
-  putStr "Verify… "
-  putStrLn $ if verified then "OK" else failures ++ "\nFail!"
+verify = I $ \c s -> do
+  (verified, failures) <- runWriterT (verification s)
+  view logger c $ (if verified then green "OK" else vcat failures) <> line
 
 
-f :: [IL] -> WriterT String IO Bool
-f = foldl' (\a -> liftA2 (&&) a . g) (return True)
+verification :: [IL] -> WriterT [TermDoc] IO Bool
+verification = foldl' (\a -> liftA2 (&&) a . go) (return True)
+ where
+  go i = case log i of
+    Just l  -> liftIO (correct i `mplus` return False) >>= \c -> unless c (tell [l]) >> return c
+    Nothing -> return True
 
+correct :: IL -> IO Bool
+correct (IS p _ _ _ _ _) = doesDirectoryExist p
+correct (IA (Link s d) _ _ _) = do
+  s' <- readSymbolicLink d
+  dfe <- doesFileExist s'
+  dde <- doesDirectoryExist s'
+  return $ s == s' && (dfe || dde)
+correct (IA (Copy s d) _ _ _) = do
+  s' <- B.readFile s
+  d' <- B.readFile d
+  return $ s' == d'
+correct (IA (Template _ d _) _ _ _) = doesFileExist d
+correct _ = return True
 
-g :: IL -> WriterT String IO Bool
-g (IS p _ _ _ _ n) = do
-  sourceExists <- liftIO $ doesDirectoryExist p
-  unless sourceExists $ tellLn [indent 2, "Source ", n, " -> ", p, " doesn't exist"]
-  return sourceExists
-g (IA (Link src dst) _ _ _) = do
-  src' <- liftIO $ readSymbolicLink dst
-  dstExists <- liftIO $ liftA2 (||) (doesFileExist src') (doesDirectoryExist src')
-  let correctLink = src == src' && dstExists
-  unless correctLink $ tellLn [indent 4, "Link at ", dst, " is broken"]
-  return correctLink
-g (IA (Copy src dst) _ _ _) = do
-  src' <- liftIO $ B.readFile src
-  dst' <- liftIO $ B.readFile dst
-  let same = src' == dst'
-  unless same $ tellLn [indent 4, "Files at ", src, " and ", dst, " are not copies"]
-  return same
-g (IA (Template _ dst _) _ _ _) = liftIO $ doesFileExist dst
-g (IA (Shell {}) _ _ _) = return True
-g (IW {}) = return True
-
-
-tellLn :: [String] -> WriterT String IO ()
-tellLn failure = tell . mconcat $ failure ++ ["\n"]
-
-
-indent :: Int -> String
-indent n = replicate n ' '
+log :: IL -> Maybe TermDoc
+log (IS p t _ _ _ u) =
+  Just $ text t </> "source" </> parens (cyan (text u)) </> "does not exist at" </> magenta (text p)
+log (IA (Link src dst) _ _ _) =
+  Just . indent 2 $ yellow (text dst) </> "link to" </> magenta (text src) </> "is broken"
+log (IA (Copy src dst) _ _ _) = do
+  Just . indent 2 $ yellow (text dst) </> "is not a copy of" </> magenta (text src)
+log (IA (Template src dst _) _ _ _) =
+  Just . indent 2 $ yellow (text dst) </> "is not a templated copy teplates of" </> magenta (text src)
+log _ = Nothing
