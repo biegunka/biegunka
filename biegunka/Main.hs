51ad75b395881c8734d90d3c98dd10077f3c0556
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-06-19 11:08:00 +0400

    Copy template to current directory in biegunka init.

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -1,9 +1,12 @@
 module Main where
 
+import System.Directory (copyFile)
+
 import Paths_biegunka_core
 
 
 main :: IO ()
 main = do
   fp <- getDataFileName "data/biegunka-init.template"
-  print fp
+  copyFile fp "Main.hs"
+  putStrLn "Initialized biegunka script at Biegunka.hs"
