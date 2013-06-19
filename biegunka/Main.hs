ce143a157fe01cb7569954bdb0776705a4c0d78c
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-06-19 18:43:13 +0400

    Warn on existing biegunka script.

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -1,12 +1,44 @@
 module Main where
 
-import System.Directory (copyFile)
+import Data.Char (toLower)
+import System.Directory (copyFile, doesFileExist)
+import System.Exit (ExitCode(..), exitWith)
+import System.IO (hFlush, stdout)
 
 import Paths_biegunka_core
 
 
+destination :: FilePath
+destination = "Main.hs"
+
+
 main :: IO ()
 main = do
-  fp <- getDataFileName "data/biegunka-init.template"
-  copyFile fp "Main.hs"
-  putStrLn "Initialized biegunka script at Biegunka.hs"
+  template <- getDataFileName "data/biegunka-init.template"
+  destinationExists <- doesFileExist destination
+  case destinationExists of
+    True -> do
+      response <- prompt "Main.hs already exists! Overwrite?"
+      case response of
+        True  -> initialize template
+        False -> do
+          putStrLn $ "Failed to initialize biegunka script: Already Exists"
+          exitWith (ExitFailure 1)
+    False -> initialize template
+
+initialize :: FilePath -> IO ()
+initialize source = do
+  copyFile source destination
+  putStrLn $ "Initialized biegunka script at " ++ destination
+
+
+prompt :: String -> IO Bool
+prompt message = do
+  putStr $ message ++ " [y/N] "
+  hFlush stdout
+  response <- getLine
+  case map toLower response of
+    "y" -> return True
+    "n" -> return False
+    ""  -> return False
+    _   -> prompt message
