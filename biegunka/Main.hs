cba84b6bf83754f0b71d4224b93ee2371584fc7d
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-06-21 15:06:26 +0400

    Support all possible run variants.

--- biegunka/Main.hs
+++ biegunka/Main.hs
@@ -11,6 +11,7 @@ import           Control.Concurrent (forkIO)
 import           Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
 import           Control.Monad (forever)
 import           Data.Char (toLower)
+import           Data.Foldable (asum)
 import           Data.List (partition)
 import           Data.Monoid (mempty)
 import qualified Data.Text.Lazy.IO as T
@@ -27,7 +28,9 @@ data BiegunkaCommand
   = Init
   | Script Script [String]
 
-data Script = DryRun | Run | Check
+data Script = Run Run | Check
+
+data Run = Dry | Safe | Force
 
 
 opts :: ParserInfo BiegunkaCommand
@@ -35,14 +38,18 @@ opts = info (helper <*> subcommands) fullDesc
  where
   subcommands = subparser $
     command "init" (info (pure Init) (progDesc "Initialize biegunka script")) <>
-    command "dry-run"  (info (Script DryRun <$> runCommandParser)
-      (progDesc "Run biegunka script dry")) <>
-    command "run"  (info (Script Run <$> runCommandParser)
+    command "run"  (info (Script <$> (Run <$> runVariant) <*> otherArguments)
       (progDesc "Run biegunka script")) <>
-    command "check"  (info (Script Check <$> runCommandParser)
+    command "check"  (info (Script Check <$> otherArguments)
       (progDesc "Check biegunka script"))
    where
-    runCommandParser = arguments Just mempty
+    runVariant = asum
+      [ flag' Dry   (long "dry"   <> help "Do not do anything, only display logs and stats")
+      , flag' Force (long "force" <> help "Do not ask for confirmation")
+      , pure Safe
+      ]
+
+    otherArguments = arguments Just mempty
 
 
 main :: IO ()
@@ -86,6 +93,7 @@ withScript script args = do
     Nothing
     Nothing
   hSetBuffering stdin' NoBuffering
+  -- Can't use waitForProcess here because runhaskell ignores -threaded
   stdoutAnchor <- newEmptyMVar
   stderrAnchor <- newEmptyMVar
   listen stdoutAnchor stdout'
@@ -102,9 +110,10 @@ withScript script args = do
 
 
 toOption :: Script -> String
-toOption Run    = "--safe-run"
-toOption DryRun = "--dry-run"
-toOption Check  = "--check"
+toOption (Run Force) = "--run"
+toOption (Run Safe)  = "--safe-run"
+toOption (Run Dry)   = "--dry-run"
+toOption Check       = "--check"
 
 
 prompt :: String -> IO Bool
