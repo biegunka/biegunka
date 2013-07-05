f1d322c337fa8408369057be4855b8c8d2b7f873
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-07-05 17:08:27 +0400

    Cosmetics.

--- spec/typecheck/typecheck_spec.rb
+++ spec/typecheck/typecheck_spec.rb
@@ -4,25 +4,41 @@
 require "rspec"
 
 
+def compile file, opts
+  system("ghc -fno-code #{opts.join(" ")} #{file} 1>/dev/null 2>&1")
+end
+
+def successes root
+  Dir.glob "#{root}/should_compile/*"
+end
+
+def failures root
+  Dir.glob "#{root}/should_fail/*"
+end
+
+
 options = []
-directory = File.expand_path File.dirname __FILE__
+here = File.expand_path File.dirname __FILE__
 if File.exists? "cabal-dev"
   options << "-package-db=#{Dir.glob("cabal-dev/packages-*.conf").first}"
 elsif File.exists? ".cabal-sandbox"
   options << "-package-db=#{Dir.glob(".cabal-sandbox/*-packages.conf.d").first}"
 end
 
-describe "typecheck:" do
-  Dir.glob("#{directory}/should_compile/*").each do |test|
-    it "should succeed to compile #{test}" do
-      system("ghc -fno-code #{options.join(" ")} #{test} 1>/dev/null 2>&1").should be_true
+describe "typechecking," do
+  context "when successful," do
+    successes(here).each do |success|
+      it "succeeds to compile #{success}" do
+        compile(success, options).should be_true
+      end
     end
   end
 
-  failures = "/home/maksenov/git/biegunka-core/tests/typecheck/should_fail"
-  Dir.glob("#{directory}/should_fail/*").each do |test|
-    it "should fail to compile #{test}" do
-      system("ghc -fno-code #{options.join(" ")} #{test} 1>/dev/null 2>&1").should be_false
+  context "when unsuccessful," do
+    failures(here).each do |failure|
+      it "fails to compile #{failure}" do
+        compile(failure, options).should be_false
+      end
     end
   end
 end
