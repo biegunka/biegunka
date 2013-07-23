7d847ca3d0e69025fcfed86d299869e2fb3e1e39
Matvey Aksenov <matvey.aksenov@gmail.com>
2013-07-23 19:29:17 +0400

    Error logging customizable formatting.

--- bin/biegunka/List.hs
+++ bin/biegunka/List.hs
@@ -1,6 +1,6 @@
 module List where
 
-import           Control.Applicative ((<$>))
+import           Control.Applicative ((<$>), (<*>))
 import           Control.Lens hiding ((<.>))
 import           Data.Char (toUpper)
 import           Data.Default (def)
@@ -11,6 +11,7 @@ import           Data.Monoid (Monoid(..), (<>))
 import           Data.Traversable (for)
 import qualified System.Directory as D
 import           System.FilePath ((</>))
+import           System.IO (hPutStrLn, stderr)
 import           System.FilePath.Lens
 import           System.Wordexp (wordexp, nosubst, noundef)
 
@@ -41,15 +42,21 @@ list datadirglob profiles pattern = do
           getProfiles (datadir </> "profiles/") >>= mapM_ putStrLn
         profiles' -> do
           DB db <- load (def & appData .~ datadir) profiles'
-          let formatted = format pattern
-          ifor_ db $ \profileName profileData -> do
-            putStrLn $ profileFormat formatted profileName
-            ifor_ profileData $ \sourceRecord fileRecords -> do
-              putStrLn $ sourceFormat formatted sourceRecord
-              for_ fileRecords $ \fileRecord ->
-                putStrLn $ fileFormat formatted fileRecord
+          case format pattern of
+            Left errorMessage ->
+              badformat errorMessage
+            Right formatted ->
+              ifor_ db $ \profileName profileData -> do
+                putStrLn $ profileFormat formatted profileName
+                ifor_ profileData $ \sourceRecord fileRecords -> do
+                  putStrLn $ sourceFormat formatted sourceRecord
+                  for_ fileRecords $ \fileRecord ->
+                    putStrLn $ fileFormat formatted fileRecord
  where
-  badglob = putStrLn $ "Bad glob pattern: " ++ datadirglob
+  badglob = hPutStrLn stderr $
+    "Bad glob pattern: " ++ datadirglob
+  badformat message = hPutStrLn stderr $
+    "Bad format pattern: " ++ pattern ++ " - " ++ message
 
 getProfiles :: FilePath -> IO [String]
 getProfiles root = go root <&> \profiles -> profiles^..folded.prefixed root & sort
@@ -64,37 +71,42 @@ getProfiles root = go root <&> \profiles -> profiles^..folded.prefixed root & so
         contents <- D.getDirectoryContents subroot <&> filter (`notElem` [".", ".."])
         concat <$> for contents (\path -> go (subroot </> path))
 
-format :: String -> Formatted String
+format :: String -> Either String (Formatted String)
 format xs =
   let (x, ys) = break (== ';') xs
       (y, zs) = break (== ';') (drop 1 ys)
       z       = drop 1 zs
-  in Formatted (formatProfile x) (formatSource y) (formatFile z)
+  in Formatted <$> formatProfile x <*> formatSource y <*> formatFile z
  where
-  formatProfile us profile = case us of
+  formatProfile :: String -> Either String (String -> String)
+  formatProfile us = case us of
     '%':vs -> case vs of
-      'n':ws -> profile ++ formatProfile ws profile
-      _ -> error "P"
-    v:vs -> v : formatProfile vs profile
-    []   -> ""
-  formatSource us source = case us of
+      'n':ws -> (\f profile -> profile ++ f profile) <$> formatProfile ws
+      w:_    -> Left ("%" ++ [w] ++ " is not a placeholder")
+      _      -> Left ("incomplete %-placeholder at the end")
+    v:vs -> (\f r -> v : f r) <$> formatProfile vs
+    []   -> Right (const "")
+  formatSource :: String -> Either String (SourceRecord -> String)
+  formatSource us = case us of
     '%':vs -> case vs of
-      't':ws -> sourceType   source ++ formatSource ws source
-      'l':ws -> fromLocation source ++ formatSource ws source
-      'p':ws -> sourcePath   source ++ formatSource ws source
-      _ -> error "S"
-    v:vs -> v : formatSource vs source
-    []   -> ""
-  formatFile us file = case us of
+      't':ws -> (\f source -> sourceType   source ++ f source) <$> formatSource ws
+      'l':ws -> (\f source -> fromLocation source ++ f source) <$> formatSource ws
+      'p':ws -> (\f source -> sourcePath   source ++ f source) <$> formatSource ws
+      w:_    -> Left ("%" ++ [w] ++ " is not a placeholder")
+      _      -> Left ("incomplete %-placeholder at the end")
+    v:vs -> (\f r -> v : f r) <$> formatSource vs
+    []   -> Right (const "")
+  formatFile :: String -> Either String (FileRecord -> String)
+  formatFile us = case us of
     '%':vs -> case vs of
-      't':ws -> fileType             file  ++ formatFile ws file
-      'T':ws -> capitalize (fileType file) ++ formatFile ws file
-      'l':ws -> fromSource           file  ++ formatFile ws file
-      'p':ws -> filePath             file  ++ formatFile ws file
-      '%':ws -> '%' : formatFile ws file
-      _ -> error "F"
-    v:vs -> v : formatFile vs file
-    []   -> ""
+      't':ws -> (\f file -> fileType             file  ++ f file) <$> formatFile ws
+      'T':ws -> (\f file -> capitalize (fileType file) ++ f file) <$> formatFile ws
+      'l':ws -> (\f file -> fromSource           file  ++ f file) <$> formatFile ws
+      'p':ws -> (\f file -> filePath             file  ++ f file) <$> formatFile ws
+      w:_    -> Left ("%" ++ [w] ++ " is not a placeholder")
+      _      -> Left ("incomplete %-placeholder at the end")
+    v:vs -> (\f r -> v : f r) <$> formatFile vs
+    []   -> Right (const "")
 
 capitalize :: String -> String
 capitalize (c:cs) = toUpper c : cs
