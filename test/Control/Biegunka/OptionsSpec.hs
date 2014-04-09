48f488fcb84518dd4218dbf7c759edaa601eeecd
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-04-09 14:27:58 +0400

    Update the test-suite to optparse-applicative-0.8

--- test/Control/Biegunka/OptionsSpec.hs
+++ test/Control/Biegunka/OptionsSpec.hs
@@ -38,50 +38,47 @@ spec = do
     let constructorOption' = snd . constructorOption mempty
 
     it "returns a parser that parses simple constructor option" $
-      parse (constructorOption' (Foo 4)) ["--foo"] `shouldHave` _Right.only (Foo 4)
+      parse (constructorOption' (Foo 4)) ["--foo"] `shouldHave` _Just.only (Foo 4)
 
     it "returns a parser that parses complex constructor option" $
-      parse (constructorOption' FooBarBaz) ["--foo-bar-baz"] `shouldHave` _Right.only FooBarBaz
+      parse (constructorOption' FooBarBaz) ["--foo-bar-baz"] `shouldHave` _Just.only FooBarBaz
 
   describe "constructorOptions" $ do
     let f = constructorOptions [Foo 1, Foo 2, Foo 3, FooBarBaz]
 
     it "returns a parser that parses simple constructor option" $
-      parse f ["--foo"] `shouldHave` _Right.only (Foo 1)
+      parse f ["--foo"] `shouldHave` _Just.only (Foo 1)
 
     it "returns a parser that parses complex constructor option" $
-      parse f ["--foo-bar-baz"] `shouldHave` _Right.only FooBarBaz
+      parse f ["--foo-bar-baz"] `shouldHave` _Just.only FooBarBaz
 
     it "adds a suffix for a second encounter of the same constructor" $
-      parse f ["--foo-1"] `shouldHave` _Right.only (Foo 2)
+      parse f ["--foo-1"] `shouldHave` _Just.only (Foo 2)
 
     it "adds a suffix for a third encounter of the same constructor" $
-      parse f ["--foo-2"] `shouldHave` _Right.only (Foo 3)
+      parse f ["--foo-2"] `shouldHave` _Just.only (Foo 3)
 
   describe "parser" $ do
     let f = parser [Foo 4, FooBarBaz]
 
     it "returns a parser that parses simple constructor option" $
-      parse f ["--foo"] `shouldHave` _Right._1.only (Foo 4)
+      parse f ["--foo"] `shouldHave` _Just._1.only (Foo 4)
 
     it "returns a parser that parses complex constructor option" $
-      parse f ["--foo-bar-baz"] `shouldHave` _Right._1.only FooBarBaz
+      parse f ["--foo-bar-baz"] `shouldHave` _Just._1.only FooBarBaz
 
     context "interpreters" $
       for_ ["--changes", "--run", "--problems", "--force", "--all"] $ \o ->
         it (printf "returns a parser that has %s option" o) $
-          parse f [o, "--foo"] `shouldHave` _Right
+          parse f [o, "--foo"] `shouldHave` _Just
 
     context "modes" $
       for_ ["--online", "--offline"] $ \o ->
         it (printf "returns a parser that has %s option" o) $
-          parse f [o, "--foo"] `shouldHave` _Right
+          parse f [o, "--foo"] `shouldHave` _Just
 
-parse :: Parser a -> [String] -> Either ParserFailure a
-parse p = execParserPure (prefs mempty) (info p fullDesc)
-
-instance Show ParserFailure where
-  show _ = "<ParserFailure>"
+parse :: Parser a -> [String] -> Maybe a
+parse p = customExecParserMaybe (prefs mempty) (info p fullDesc)
 
 instance Show (a -> b) where
   show _ = "<Function>"
