ca988efd760d18790c2d7311b5fe24776388d432
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-07-07 14:21:57 +0300

    It's actually *the* getter, IIRC

--- src/Control/Biegunka/Source.hs
+++ src/Control/Biegunka/Source.hs
@@ -11,14 +11,14 @@ import Control.Biegunka.Script (Url)
 
 -- | Types that contain an URL.
 --
--- Having only a setter simplifies the interface, so we omit a getter,
+-- Having only a setter simplifies the interface, so we omit the getter,
 -- as the user is not supposed to look into the configuration.
 class HasUrl s t a | s -> a, t -> a, a s -> t where
   url :: a -> s -> t
 
 -- | Types that contain a file path.
 --
--- Having only a setter simplifies the interface, so we omit a getter,
+-- Having only a setter simplifies the interface, so we omit the getter,
 -- as the user is not supposed to look into the configuration.
 class HasPath s t a | s -> a, t -> a, a s -> t where
   path :: a -> s -> t
