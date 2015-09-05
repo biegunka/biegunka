aa1515dfec51f6468475c13828b5119f4eb2b9d1
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-09-05 18:14:43 +0300

    Update the compiler version to build the project with.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./biegunka.nix {
   mkDerivation = args: nixpkgs.pkgs.haskell.packages.${compiler}.mkDerivation(args // {
     buildTools = (if args ? buildTools then args.buildTools else []) ++ [ nixpkgs.pkgs.git ];
